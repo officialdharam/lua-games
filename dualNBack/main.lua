a139358e46f2f594d545fb2a563bd7ef125b132a
Dharmendra <officialdharam@gmail.com>
2019-10-20 23:39:41 +0200

    fixing time issues

--- dualNBack/main.lua
+++ dualNBack/main.lua
@@ -24,13 +24,14 @@ function love.update(dt)
     hiddenTime = hiddenTime + 1
   end
 
-  if(hiddenTime == 60) then
+  if(hiddenTime >= 40) then
     indexToShow = love.math.random( max )
     hiddenTime = 0
     showing = true
   end
-  if(showingTime == 60) then
+  if(showingTime >= 60) then
     showing = false
+    showingTime = 0
   end
 end
 
@@ -38,13 +39,12 @@ function love.draw()
 
     love.graphics.print("Welcome to Dual N Back", 100, 50)
     love.graphics.setColor(unpack(color))
-    for x = 1,num do
-        for y = 1,num do
-
-            love.graphics.rectangle("fill", X_OFFSET +(x - 1)*(w) ,Y_OFFSET +(y - 1)*(h), w-10, h-10)
-            res = x * num + y;
+    for x = 0, num-1 do
+        for y = 0, num-1 do
+            love.graphics.rectangle("fill", X_OFFSET +(x)*(w) ,Y_OFFSET +(y)*(h), w-10, h-10)
+            res = (x * num + y) + 1;
             if(res == indexToShow and not showing) then
-              love.graphics.draw(image, X_OFFSET +(x - 1)*(w)+ 20 ,Y_OFFSET +(y - 1)*(h)+20)
+              love.graphics.draw(image, X_OFFSET +(x)*(w)+ 20 ,Y_OFFSET +(y)*(h)+20)
             end
         end
     end
